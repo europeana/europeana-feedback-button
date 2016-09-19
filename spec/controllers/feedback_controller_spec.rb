f67b602190fd6f72382a602bdbafb116f48b2b31
Lutz Biedinger <lutz.biedinger@gmail.com>
2016-09-19 18:10:11 +0200

    remove delayed job

--- spec/controllers/feedback_controller_spec.rb
+++ spec/controllers/feedback_controller_spec.rb
@@ -1,6 +1,10 @@
 RSpec.describe Europeana::FeedbackButton::FeedbackController do
+  def main_app
+    Rails.application.class.routes.url_helpers
+  end
+
   describe 'POST create' do
-    let(:params) { { locale: 'en', type: 'comment', text: 'This is good!', url: home_url(locale: 'en') } }
+    let(:params) { { locale: 'en', type: 'comment', text: 'This is good, five words!', url: main_app.root_path(locale: 'en') } }
     subject { post :create, params }
 
     context 'with recipient configured' do
@@ -8,9 +12,16 @@ RSpec.describe Europeana::FeedbackButton::FeedbackController do
         Rails.application.config.x.feedback_mail_to = 'feedback@example.com'
       end
 
-      it 'should queue an email job' do
-        expect { subject }.to change { Delayed::Job.where("handler LIKE '%FeedbackMailer%'").count }.by(1)
+      it "should queue an email job" do
+        expect { subject }.to change(ActionMailer::Base.deliveries, :length)
       end
+      #it '' do
+      #  message_delivery = instance_double(ActionMailer::MessageDelivery)
+      #  expect(ServiceMailer).to receive(:new_user).with(@user).and_return(message_delivery)
+      #  expect(message_delivery).to receive(:deliver_later)
+      #  subject
+        #expect { subject }.to change { Delayed::Job.where("handler LIKE '%FeedbackMailer%'").count }.by(1)
+      #end
     end
 
     context 'without recipient configured' do
@@ -19,7 +30,7 @@ RSpec.describe Europeana::FeedbackButton::FeedbackController do
       end
 
       it 'should not queue an email job' do
-        expect { subject }.to_not change { Delayed::Job.where("handler LIKE '%FeedbackMailer%'").count }
+        expect { subject }.to_not change(ActionMailer::Base.deliveries, :length)
       end
     end
   end
